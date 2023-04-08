bad6971ca666a1a8555daad7e1eb56bb8d613874
Zubbbz <37497090+Zubbbz@users.noreply.github.com>
2023-04-08 19:45:09 -0400

    turn image saving into a function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
 	fs::File,
-	io::{BufWriter},
-	path::Path,
+	io::{BufWriter, Write},
+	path::{Path, PathBuf},
 };
 
 use png::{BitDepth, ColorType, Encoder, ScaledFloat};
@@ -13,8 +13,8 @@ fn main() {
 	let mut rgb_buffer: Vec<u8> = vec![];
 
 	for y in (0..IMG_HEIGHT).rev() {
-		// eprint!("\rLines Remaining: {} ", y);
-		// std::io::stderr().flush().unwrap();
+		eprint!("\rLines Remaining: {} ", y);
+		std::io::stderr().flush().unwrap();
 
 		for x in 0..IMG_WIDTH {
 			let r = (x as f64) / (IMG_WIDTH - 1) as f64;
@@ -27,17 +27,49 @@ fn main() {
 		}
 	}
 
-	let path = std::env::current_dir()
-		.unwrap()
-		.join(Path::new("./image.png"));
-	let file = File::create(path).unwrap();
-	let ref mut w = BufWriter::new(file);
-	let mut encoder = Encoder::new(w, IMG_WIDTH, IMG_HEIGHT);
-	encoder.set_color(ColorType::Rgb);
-	encoder.set_depth(BitDepth::Eight);
-	encoder.set_source_gamma(ScaledFloat::new(1.0 / 2.2));
-	let mut writer = encoder.write_header().unwrap();
-	writer.write_image_data(&rgb_buffer).unwrap();
+	// let path = std::env::current_dir()
+	// 	.unwrap()
+	// 	.join(Path::new("./image.png"));
+	// let file = File::create(path).unwrap();
+	// let ref mut w = BufWriter::new(file);
+	// let mut encoder = Encoder::new(w, IMG_WIDTH, IMG_HEIGHT);
+	// encoder.set_color(ColorType::Rgb);
+	// encoder.set_depth(BitDepth::Eight);
+	// encoder.set_source_gamma(ScaledFloat::new(1.0 / 2.2));
+	// let mut writer = encoder.write_header().unwrap();
+	// writer.write_image_data(&rgb_buffer).unwrap();
+	save_image(None, &rgb_buffer).unwrap();
 
 	println!("\nDone.\n");
 }
+
+fn save_image(
+	path: Option<PathBuf>,
+	image_buffer: &Vec<u8>,
+) -> Result<(), png::EncodingError> {
+	// TODO: just gonna keep it as rgb for now, I might make it a parameter later but there isn't really a point
+	let file_path = path.unwrap_or(
+		std::env::current_dir()
+			.unwrap()
+			.join(Path::new("./image.png")),
+	);
+	let file = File::create(file_path).unwrap();
+
+	let ref mut w = BufWriter::new(file);
+	let mut encoder = Encoder::new(w, IMG_WIDTH, IMG_HEIGHT);
+
+	encoder.set_color(ColorType::Rgb);
+	encoder.set_depth(BitDepth::Eight);
+	encoder.set_source_gamma(ScaledFloat::new(1.0 / 2.2));
+
+	let mut writer = encoder.write_header().unwrap();
+	match writer.write_image_data(image_buffer) {
+		Ok(()) => Ok(()),
+		Err(err) => Err(err),
+	}
+}
+
+	writer.write_image_data(&rgb_buffer).unwrap();
+
+	println!("\nDone.\n");
+	}
